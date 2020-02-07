90978c72a012765fd595f62a25ab46b547b07de8
Jamie Adams <adams.jamie@bitsourceky.com>
2020-02-07 00:29:32 -0500

    added article to the site

---
+++ src/templates/article.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { graphql } from 'gatsby';
+
+import Layout from '../compontents/layout';
+
+const Article = ({ data }) => {
+	const post = data.nodeArticle;
+
+	return (
+		<Layout>
+			<h1>{post.title}</h1>
+
+			<img 
+				src={data.nodeArticle.relationship.field_image.localFile.publicURL}
+				alt={data.nodeArticle.relationship.field_image.alt}
+			/>
+
+			<div
+				dangerouslySetInnerHtml={{_html: post.body.processed}}
+			/>
+		</Layout>
+	);
+};
+
+Article.propTypes = {
+	data: PropTypes.object.isRequired,
+};
+
+export const query = graphql`
+	query($ArticleId: String!){
+		nodeArticle(id: { eq: $ArticleId }) {
+			id
+			title
+			body {
+				processed
+			}
+			field_image {
+				alt
+			}
+			relationships {
+				field_image {
+					localFile {
+						publicURL
+					}
+				}
+			}
+		}
+	}
