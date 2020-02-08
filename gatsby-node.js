cb0b4522da2ce816616a6b05a1195cea3a1b6aa1
Jamie Adams <adams.jamie@bitsourceky.com>
2020-02-08 02:44:16 -0500

    added article to the site

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,31 +3,3 @@
  *
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
-
-const path = require(`path`);
-
-exports.createPages = ({ graphql, actions }) => {
-    const { createPage } = actions;
-    return graphql(`
-    {
-     allNodeArticle {
-       edges {
-         node {
-           id
-         }
-       }
-     }
-    }
-  `
-    ).then(result => {
-        result.data.allNodeArticle.edges.forEach(({ node }) => {
-            createPage({
-                path: node.id,
-                component: path.resolve(`./src/templates/article.js`),
-                context: {
-                    id: node.id,
-                },
-            })
-        })
-    })
-}
--- src/pages/articles.js
+++ src/pages/articles.js
@@ -1,15 +1,63 @@
 import React from "react"
-import { Link } from "gatsby"
-
+import { graphql } from "gatsby"
 import Layout from "../components/layout"
-import SEO from "../components/seo"
+import Img from 'gatsby-image'
 
-const ArticlesPage = () => (
-    <Layout>
-        <SEO title="Articles" />
-        <h1>Articles</h1>
-        <Link to="/">Go back to the homepage</Link>
-    </Layout>
-)
+export default ({ data }) => {
+    console.log(data)
+    return (
+        <Layout>
+            <div>
+                <h1>My Site's Files</h1>
+                <table>
+                    <thead>
+                    <tr>
+                        <th>image</th>
+                        <th>title</th>
+                        <th>body</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {data.allNodeArticle.edges.map(({ node }, index) => (
+                        <tr key={index}>
+                            <td><div style={{ maxWidth: `900px`, marginBottom: `1.45rem`, width: `100%` }}>
+                                <Img fluid={ node.relationships.field_image.localFile.childImageSharp.fluid } />
+                            </div></td>
+                            <td>{node.title}</td>
+                            <td>{node.body['value']}</td>
+                        </tr>
+                    ))}
+                    </tbody>
+                </table>
+            </div>
+        </Layout>
+    )
+}
 
-export default ArticlesPage
+export const query = graphql`
+  query {
+    allNodeArticle {
+      edges {
+        node {
+          id
+          title
+          body{
+            value
+          }
+          created
+          relationships {
+            field_image {
+              localFile {
+                childImageSharp {
+                  fluid(maxWidth: 400, quality: 100) {
+                    ...GatsbyImageSharpFluid
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
